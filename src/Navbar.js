055e39ec004f114f9309276fb6ab75991afa6852
vagnerassilva <vagner.santossilva@hotmail.com>
2021-07-05 13:23:08 -0300

    primeiros passos

---
+++ src/Navbar.js
@@ -0,0 +1,44 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Aluga Mais
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/">
+                Home
+              </NavLink>
+            </li>
+            {/* <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/new-character"
+              >
+                New Character
+              </NavLink>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+function Home() {
+    return (
+        <div>
+            <h1>Aluga Mais</h1>
+            <h3>Aluguel de veículos particulares.</h3>
+            <hr/>
+            <h2>Carrossel</h2>
+        </div>
+    )
+
+}
+
+export default Home;
