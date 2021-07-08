a0c8298666524ff1573fa09d5d06b8545a1e7e85
vagnerassilva <vagner.santossilva@hotmail.com>
2021-07-08 10:36:31 -0300

    carrossel pronto

--- src/components/VehicleCard.js
+++ src/components/VehicleCard.js
@@ -3,38 +3,15 @@ import { Link } from 'react-router-dom';
 
 
 function VehicleCard(props) {
-  //   console.log(props);
+
+  
+
   return (
-    // <div className="card m-3" style={{ maxWidth: '540px' }}>
-    //   <div className="row g-0">
-    //     <div className="col-md-6">
-    //       <Link to={`/beers/${props._id}`}>
-    //         <img
-    //           style={{height: '10rem' }}
-    //           src={props.image_url}
-    //           className="img-fluid rounded-start"
-    //           alt="..."
-    //         />
-    //       </Link>
-    //     </div>
-    //     <div className="col-md-6">
-    //       <div className="card-body">
-    //         <h5 className="card-title">{props.name}</h5>
-    //         <p className="card-text">{props.brand}</p>
-    //         <p className="card-text">
-    //           <small className="text-muted">{props.type}</small>
-    //         </p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-
         
             <div className="col">
                 <div className="card h-100">
                 <Link to={`/alugaMais/details/${props._id}`}>
-                <img src={props.image_url} className="card-img-top" alt="vehicle" /></Link>
+                <img src={props.image_url} style={props.style}  className="card-img-top" alt="vehicle" /></Link>
                 <div className="cardBody">
                     <h5 className="cardTitle">{props.name}</h5>
                     <p className="cardText">{props.brand}</p>
@@ -71,10 +48,7 @@ function VehicleCard(props) {
 
                 </div>
                 </div>
-            </div>
-
-            
-        
+            </div>    
   );
 }
 
